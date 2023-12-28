21823e64b6d77e7616b9e4df2eea0551117a35e9
haticeadiguzel <adiguzell.hatice@gmail.com>
2023-12-28 17:05:11 +0300

    spring boot created

---
+++ src/test/java/api/response/apiResponse/ApiResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package api.response.apiResponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
