ea8831ae5266a4577e7144680f0f14a9a131b956
haticeadiguzel <adiguzell.hatice@gmail.com>
2023-12-30 13:01:31 +0300

    Db saving updated

--- src/main/java/api/response/apiResponse/business/abstracts/AddressService.java
+++ src/main/java/api/response/apiResponse/business/abstracts/AddressService.java
@@ -1,7 +1,9 @@
 package api.response.apiResponse.business.abstracts;
 
 import api.response.apiResponse.business.DTOs.Responses.GetAllAddressesResponse;
+import api.response.apiResponse.entities.concretes.Address;
 
 public interface AddressService {
     GetAllAddressesResponse getAddressesData();
+    Address convertToAddressEntity(GetAllAddressesResponse addressResponse);
 }
--- src/main/java/api/response/apiResponse/business/concretes/AddressManager.java
+++ src/main/java/api/response/apiResponse/business/concretes/AddressManager.java
@@ -2,9 +2,9 @@ package api.response.apiResponse.business.concretes;
 
 import api.response.apiResponse.business.DTOs.Responses.GetAllAddressesResponse;
 import api.response.apiResponse.business.abstracts.AddressService;
+import api.response.apiResponse.core.utilities.mappers.ModelMapperService;
 import api.response.apiResponse.dataAccess.abstracts.AddressRepository;
 import api.response.apiResponse.entities.concretes.Address;
-import api.response.apiResponse.entities.concretes.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -13,14 +13,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @Slf4j
 public class AddressManager implements AddressService {
     @Autowired
     private AddressRepository addressRepository;
+    @Autowired
+    private ModelMapperService modelMapperService;
     @Value("${address.url}")
     private String url;
 
@@ -28,7 +27,6 @@ public class AddressManager implements AddressService {
     public GetAllAddressesResponse getAddressesData() {
         try {
             WebClient.Builder builder = WebClient.builder();
-
             GetAllAddressesResponse address = builder.build()
                     .get()
                     .uri(url)
@@ -38,7 +36,6 @@ public class AddressManager implements AddressService {
 
             Address addressEntity = convertToAddressEntity(address);
             addressRepository.save(addressEntity);
-
             log.info("Output from api: {}", address);
 
             return address;
@@ -51,33 +48,10 @@ public class AddressManager implements AddressService {
         }
     }
 
-    private Address convertToAddressEntity(GetAllAddressesResponse addressResponse) {
-        // Bir Address entity oluştur ve özelliklerini ayarla
-        Address addressEntity = new Address();
-        addressEntity.setTotalCount(addressResponse.getTotalCount());
-        addressEntity.setCount(addressResponse.getCount());
-        addressEntity.setModels(convertToModelEntities(addressResponse.getModels()));
-        addressEntity.setPage(addressResponse.getPage());
-        addressEntity.setPageCount(addressResponse.getPageCount());
-
-        return addressEntity;
-    }
-
-    private List<Model> convertToModelEntities(List<Model> models) {
-        // Model DTO'ların bulunduğu listeyi Model entity'lerin bulunduğu liste olarak çevir
-        return models.stream()
-                .map(modelDto -> {
-                    Model modelEntity = new Model();
-                    modelEntity.setId(modelDto.getId());
-                    modelEntity.setUrl(modelDto.getUrl());
-                    modelEntity.setType(modelDto.getType());
-                    modelEntity.setDesc(modelDto.getDesc());
-                    modelEntity.setSource(modelDto.getSource());
-                    modelEntity.setDate(modelDto.getDate());
-                    modelEntity.setCriticality_level(modelDto.getCriticality_level());
-                    modelEntity.setConnectiontype(modelDto.getConnectiontype());
-                    return modelEntity;
-                })
-                .collect(Collectors.toList());
+    @Override
+    public Address convertToAddressEntity(GetAllAddressesResponse addressResponse) {
+        Address address = this.modelMapperService.forResponse().map(addressResponse, Address.class);
+        log.info("Address added to DB");
+        return address;
     }
 }
